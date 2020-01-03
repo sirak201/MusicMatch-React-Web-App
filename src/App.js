918ad5b9736dff7affed63d2aab9b6794feafccf
Sirak Zeray <sirakzeray@gmail.com>
2020-01-02 19:35:22 -0500

    Added Lyrics component to display lyrics of song

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/Navbar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+
 import { Provider } from "./context";
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
